7664f4c06651478328ccabbbbef0256bb3da0a99
oseparovic <oseparovic@gmail.com>
2025-02-10 11:13:11 -0800

    storePurchasePending is now an optional interface method

--- library/src/main/java/com/appstronautstudios/universalstoremanager/utils/StoreEventListener.java
+++ library/src/main/java/com/appstronautstudios/universalstoremanager/utils/StoreEventListener.java
@@ -3,9 +3,11 @@ package com.appstronautstudios.universalstoremanager.utils;
 public interface StoreEventListener {
     void storeBillingInitialized(boolean success, int code); // error code on failure
 
-    void storePurchaseComplete(String sku); // id of completed purchase
+    default void storePurchasePending(String sku) {
+        // optional method. Do nothing by default
+    }
 
-    void storePurchasePending(String sku);
+    void storePurchaseComplete(String sku); // id of completed purchase
 
     void storePurchaseError(int errorCode);
 }
